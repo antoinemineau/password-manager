d6d3b8a32cbd3faf70c25aacd60fad14dfc733f4
Antoine Mineau <antoine.mineau@gmail.com>
2015-10-11 20:27:56 +0200

    added list of password, link to decode

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,10 @@
+
 export default Ember.Controller.extend({
   init: function() {
     var _this = this;
     YAML.load('credentials.yml', function(result)
     {
-        _this.set('credentials',result);
+        _this.set('sections',result);
     });
   }
 });

af1887cfe50205638df8c64547291d0f9243d0a5
Antoine Mineau <antoine.mineau@gmail.com>
2015-10-18 23:05:46 +0200

    parse multiple yaml

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,16 @@
-
 export default Ember.Controller.extend({
   init: function() {
+    var files = ['credentials_utils.yml', 'credentials_perso.yml', 'credentials_pro.yml', 'credentials_server.yml'];
     var _this = this;
-    YAML.load('credentials.yml', function(result)
-    {
-        _this.set('sections',result);
-    });
+    var results = [];
+
+    for (var i = 0; i < files.length; i++) {
+      YAML.load(files[i], function (result) {
+        results.push(result);
+        if (results.length === files.length) {
+          _this.set('sections', results);
+        }
+      });
+    }
   }
 });
